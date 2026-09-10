44db904035a4d150b3d8b05aaeddead9dd4a91b1
Greyrib <mlabildgaard@live.dk>
2026-09-10 17:10:06 +0200

    Add VS gitignore, and continuous NFC send + input-based test send + input to terminate

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,54 +1,126 @@
 ﻿using PCSC;
 using PCSC.Exceptions;
 using PCSC.Monitoring;
+using RabbitMQ.AMQP.Client;
+using RabbitMQ.AMQP.Client.Impl;
+using System.Text;
 
-namespace ConsoleApp
+namespace ConsoleApp;
+
+internal class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+    static bool awaitingNFCInput = true;
 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        while (awaitingNFCInput)
+        {
             Read_From_Card();
 
-            Console.WriteLine(" - - - ");
-            Console.ReadLine();
+            var input = Console.ReadKey();
+            if (input.Key == ConsoleKey.Spacebar)
+            {
+                Send_Message ("Test message.");
+            }
+
+            if (input.Key == ConsoleKey.Q)
+            {
+                Console.WriteLine ("Exit input detected - ending detection of inputs.");
+                awaitingNFCInput = false;
+            }
         }
 
-        static void Read_From_Card ()
-        {
-            using var context = ContextFactory.Instance.Establish(SCardScope.System);
+        Console.WriteLine("No longer awaiting NFC input.");
+        Console.WriteLine ("Press any key to exit the application.");
+        Console.ReadLine();
+    }
 
-            var readers = context.GetReaders();
+    static void Read_From_Card ()
+    {
+        try
+        {
+            using var context = ContextFactory.Instance.Establish (SCardScope.System);
+
+            var readers = context.GetReaders ();
 
             if (readers.Length == 0)
             {
-                Console.WriteLine("NO READER FOUND");
+                Console.WriteLine ("NO READER FOUND");
                 return;
             }
 
-            Console.WriteLine($"Reader: {readers[0]}");
-            Console.WriteLine("Waiting for card...");
+            Console.WriteLine ($"Reader: {readers[0]}");
+            Console.WriteLine ("Waiting for card...");
 
-            using var monitor = MonitorFactory.Instance.Create(SCardScope.System);
+            using var monitor = MonitorFactory.Instance.Create (SCardScope.System);
 
             monitor.CardInserted += (_, e) =>
             {
-                Console.WriteLine("CARD INSERTED");
-                Console.WriteLine($"ATR: {BitConverter.ToString(e.Atr)}");
+                Console.WriteLine ("CARD INSERTED");
+                Console.WriteLine ($"ATR: {BitConverter.ToString (e.Atr)}");
+
+                Send_Message (BitConverter.ToString (e.Atr));
             };
 
             monitor.CardRemoved += (_, _) =>
             {
-                Console.WriteLine("CARD REMOVED");
+                Console.WriteLine ("CARD REMOVED");
             };
 
-            monitor.Start(readers[0]);
-
-            Console.ReadLine();
-
-
+            monitor.Start (readers[0]);
+        }
+        catch (PCSC.Exceptions.NoServiceException)
+        {
+            Console.WriteLine ("Smart card service is not running.");
+            return;
         }
     }
+
+    static async void Send_Message (string messageContent)
+    {
+        const string brokerUri = "amqp://guest:guest@localhost:5672/%2f";
+
+        ConnectionSettings settings = ConnectionSettingsBuilder.Create ()
+            .Uri (new Uri (brokerUri))
+            .ContainerId ("tutorial-send")
+        .Build ();
+
+        IEnvironment environment = AmqpEnvironment.Create (settings);
+        IConnection connection = await environment.CreateConnectionAsync ();
+
+        try
+        {
+            IManagement management = connection.Management ();
+            IQueueSpecification queueSpec = management.Queue ("hello").Type (QueueType.QUORUM);
+            await queueSpec.DeclareAsync ();
+
+            IPublisher publisher = await connection.PublisherBuilder ().Queue ("hello").BuildAsync ();
+            try
+            {
+                //const string body = messageContent; // Gives a 'must be constant' error, kept for now for reference
+                string body = messageContent;
+                var message = new AmqpMessage (Encoding.UTF8.GetBytes (body));
+                PublishResult pr = await publisher.PublishAsync (message);
+                if (pr.Outcome.State != OutcomeState.Accepted)
+                {
+                    Console.Error.WriteLine ($"Unexpected publish outcome: {pr.Outcome.State}");
+                    Environment.Exit (1);
+                }
+
+                Console.WriteLine ($" [x] Sent {body}");
+            }
+            finally
+            {
+                await publisher.CloseAsync ();
+            }
+        }
+        finally
+        {
+            await connection.CloseAsync ();
+            await environment.CloseAsync ();
+        }
+    }
+
 }
---
+++ ConsoleApp/TestReceiver/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.AMQP.Client;
+using RabbitMQ.AMQP.Client.Impl;
+using System.Text;
+
+namespace TestReceiver;
+
+internal class Program
+{
+    static void Main (string[] args)
+    {
+        Console.WriteLine ("Hello, World!");
+
+        Receive ();
+
+        Console.WriteLine ("Awaiting exit input, press any key to terminate.");
+        Console.ReadLine ();
+    }
+
+    static async void Receive ()
+    {
+        const string brokerUri = "amqp://guest:guest@localhost:5672/%2f";
+
+        ConnectionSettings settings = ConnectionSettingsBuilder.Create ()
+            .Uri (new Uri (brokerUri))
+            .ContainerId ("tutorial-send")
+            .Build ();
+
+        IEnvironment environment = AmqpEnvironment.Create (settings);
+        IConnection connection = await environment.CreateConnectionAsync ();
+
+
+        IManagement management = connection.Management ();
+        IQueueSpecification queueSpec = management.Queue ("hello").Type (QueueType.QUORUM);
+        await queueSpec.DeclareAsync ();
+
+        IConsumer consumer = await connection.ConsumerBuilder ()
+            .Queue ("hello")
+            .MessageHandler ((ctx, message) =>
+            {
+                Console.WriteLine ($"Received a message: {Encoding.UTF8.GetString (message.Body ()!)}");
+                ctx.Accept ();
+                return Task.CompletedTask;
+            })
+            .BuildAndStartAsync ();
+    }
+}
