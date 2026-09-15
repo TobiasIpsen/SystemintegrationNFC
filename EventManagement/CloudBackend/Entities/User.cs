c15d7eb66aefa06d8b3c53edc215ef45a7b5fa80
Tobias Ipsen <71149806+TobiasIpsen@users.noreply.github.com>
2026-09-15 12:03:06 +0200

    feat: Add Cloud Backend RabbitMQ Producer (#17)
    Co-authored-by: Greyrib <144381882+Greyrib@users.noreply.github.com>

---
+++ EventManagement/CloudBackend/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudBackend.Entities
+{
+    public class User
+    {
+        public string Name { get; set; }
+        //public string Email { get; set; }
+        public string UserClass { get; set; }
+        public string CardId { get; set; }
+        public string Image { get; set; }
+    }
+}
