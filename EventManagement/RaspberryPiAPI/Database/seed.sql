98f7c1ebafe9fa32689ba89b439664fd69187482
Greyrib <144381882+Greyrib@users.noreply.github.com>
2026-09-15 14:45:30 +0200

    PostgreSQL + Seed.sql

---
+++ EventManagement/RaspberryPiAPI/Database/seed.sql
@@ -0,0 +1,17 @@
+﻿BEGIN;
+
+CREATE TABLE IF NOT EXISTS Users_XX (
+	id primary key,
+	xx,
+	xx,
+	xx,
+	xx
+);
+
+SAVEPOINT TableCreated_TestUserNotInputted;
+
+INSERT INTO Users_XX (xx,xx,xx)
+VALUES
+(xx,xx,"0000000000000000000000000000000000000000");
+
+COMMIT;
