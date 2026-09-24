28021d2e1f08c69e6c404a4f589ded8e7d4fb57e
Greyrib <mlabildgaard@live.dk>
2026-09-24 13:25:53 +0200

    Parts for making syncing add students to RaspPi Db
    NOTE: We currently accept multiple entries with same info (excluding id column) ; we can adjust this if desired
    
    - .ToString() override for Student added, for use in logging in MessageConsumer on getting a sync-message with student info
    
    - Port adjusted to work for local dev in appsettings.dev for RaspPi ; also added an out-commented section with compose-matching port, for later launching via compose

--- EventManagement/RaspberryPiAPI/RabbitMQ/MessageConsumer.cs
+++ EventManagement/RaspberryPiAPI/RabbitMQ/MessageConsumer.cs
@@ -2,11 +2,15 @@
 using CloudBackend.Entities;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.FeatureManagement;
+using Npgsql;
 using RabbitMQ.AMQP.Client;
 using RabbitMQ.AMQP.Client.Impl;
 using RaspberryPiAPI.Services;
+using System.Data.Common;
 using System.Text;
 using System.Text.Json;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RaspberryPiAPI.RabbitMQ;
 
@@ -17,15 +21,17 @@ public class MessageConsumer : BackgroundService
     /*const string brokerUri = "amqp://guest:guest@192.168.137.1:5672/%2f";*/ // For "cloud's" connection
 
     private readonly WebSocketClientManager _manager;
+    NpgsqlDataSource dataSource;
     IEventRegistrationCheckService eRegCheckService;
     IStudentData studentData;
 
-    public MessageConsumer (WebSocketClientManager manager, IEventRegistrationCheckService eRegCheckService, IStudentData studentData, IFeatureManager featureManager)
+    public MessageConsumer (WebSocketClientManager manager, NpgsqlDataSource dataSource, IEventRegistrationCheckService eRegCheckService, IStudentData studentData, IFeatureManager featureManager)
     {
         Console.WriteLine("Message Consumer was created.");
 
         _manager = manager;
         _featureManager = featureManager;
+        this.dataSource = dataSource;
         this.eRegCheckService = eRegCheckService;
         this.studentData = studentData;
     }
@@ -90,15 +96,74 @@ public class MessageConsumer : BackgroundService
 
         IConsumer cloudConsumer = await connection.ConsumerBuilder()
             .Queue("cloudsync")
-            .MessageHandler((ctx, message) =>
+            .MessageHandler(async (ctx, message) =>
             {
-                string messageContent = Encoding.UTF8.GetString(message.Body()!);
-                MessageType deserializedMessage = JsonSerializer.Deserialize<MessageType>(messageContent);
                 Console.WriteLine($"{Timestamp()} Received a cloud sync message: \n");
-                Console.WriteLine($"MessageContent {messageContent}");
+
+                string messageContent = Encoding.UTF8.GetString(message.Body()!);
+                MessageType? deserializedMessage = JsonSerializer.Deserialize<MessageType>(messageContent);
+                
+                if (deserializedMessage != null)
+                {
+                    Console.WriteLine ($"MessageContent: {deserializedMessage.Student.ToString()} and with message-timestamp: {deserializedMessage.Timestamp}");
+
+                    try
+                    {
+                        await using var checkCmd = dataSource.CreateCommand ("""
+                            SELECT EXISTS (
+                                SELECT 1 FROM students
+                                WHERE name = $1 AND userclass = $2 AND cardid = $3 AND image = $4
+                            )
+                        """);
+                        checkCmd.Parameters.AddWithValue (deserializedMessage.Student.Name);
+                        checkCmd.Parameters.AddWithValue (deserializedMessage.Student.ClassName);
+                        checkCmd.Parameters.AddWithValue (deserializedMessage.Student.CardId);
+                        checkCmd.Parameters.AddWithValue (deserializedMessage.Student.Image);
+                        bool similarStudentEntryExists = (bool)(await checkCmd.ExecuteScalarAsync ())!;
+
+
+
+                        await using var cmd = dataSource.CreateCommand ("""
+                            INSERT INTO students (name, userclass, cardid, image)
+                            VALUES ($1, $2, $3, $4)
+                        """);
+                        cmd.Parameters.AddWithValue (deserializedMessage.Student.Name);
+                        cmd.Parameters.AddWithValue (deserializedMessage.Student.ClassName);
+                        cmd.Parameters.AddWithValue (deserializedMessage.Student.CardId);
+                        cmd.Parameters.AddWithValue (deserializedMessage.Student.Image);
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync ();
+
+                        if (rowsAffected == 1)
+                        {
+                            Console.WriteLine ("Student succesfully inserted into the DB.");
+
+                            if(similarStudentEntryExists)
+                            {
+                                Console.WriteLine ("NOTE: A similar entry with matching fields (excluding id column) already existed.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine ("Something went wrong - the student wasn't inserted into the DB.");
+                        }
+                    }
+                    catch (PostgresException ex)
+                    {
+                        Console.WriteLine ($"Insert failed: {ex.MessageText} (SqlState: {ex.SqlState}, Detail: {ex.Detail})");
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        Console.WriteLine ($"DB connection/command error: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine ("MessageContent was null.");
+                }
 
                 ctx.Accept();
-                return Task.CompletedTask;
+                return; // Lambda made async (.MessageHandler above), automatically returns a task
             })
             .BuildAndStartAsync();
         #endregion
