99256c60e2c835f098f78a6185145c933ca279a9
Greyrib <mlabildgaard@live.dk>
2026-09-25 11:21:08 +0200

    Reimplemented fixes lost in stale PR-branches

--- EventManagement/RaspberryPiAPI/RabbitMQ/MessageConsumer.cs
+++ EventManagement/RaspberryPiAPI/RabbitMQ/MessageConsumer.cs
@@ -17,7 +17,9 @@ namespace RaspberryPiAPI.RabbitMQ;
 public class MessageConsumer : BackgroundService
 {
     private readonly IFeatureManager _featureManager;
-    const string brokerUri = "amqp://guest:guest@localhost:5672/%2f"; // For local testing
+    // Merges fucked stuff up ; still keeping these two for reference - will be overwritten by 
+    private readonly string brokerUri;
+    //private const string brokerUri = "amqp://guest:guest@localhost:5672/%2f"; // For local testing
     /*const string brokerUri = "amqp://guest:guest@192.168.137.1:5672/%2f";*/ // For "cloud's" connection
 
     private readonly WebSocketClientManager _manager;
@@ -25,7 +27,7 @@ public class MessageConsumer : BackgroundService
     IEventRegistrationCheckService eRegCheckService;
     IStudentData studentData;
 
-    public MessageConsumer (WebSocketClientManager manager, NpgsqlDataSource dataSource, IEventRegistrationCheckService eRegCheckService, IStudentData studentData, IFeatureManager featureManager)
+    public MessageConsumer (WebSocketClientManager manager, NpgsqlDataSource dataSource, IEventRegistrationCheckService eRegCheckService, IStudentData studentData, IFeatureManager featureManager, IConfiguration configuration)
     {
         Console.WriteLine("Message Consumer was created.");
 
@@ -34,6 +36,7 @@ public class MessageConsumer : BackgroundService
         this.dataSource = dataSource;
         this.eRegCheckService = eRegCheckService;
         this.studentData = studentData;
+        brokerUri = configuration["RabbitMQ:Uri"] ?? throw new InvalidOperationException ("Missing config: RabbitMQ:Uri");
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
